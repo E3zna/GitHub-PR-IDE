cb67a0b07a542346ffddcb34db688e548a03fd7d
3zna <3zha91@gmail.com>
2023-06-16 15:41:18 +0300

    Add subtraction

--- src/app/Main.java
+++ src/app/Main.java
@@ -12,6 +12,8 @@ public class Main {
         System.out.println(c);
         int d = multiply(a, b);
         System.out.println(d);
+        int e = subtract(a, b);
+        System.out.println(e);
     }
 
     private static int add(int a, int b) {
@@ -20,6 +22,10 @@ public class Main {
 
     private static int multiply(int a, int b) {
         return a * b;
+    }
+
+    private static int subtract(int a, int b) {
+        return a - b;
     }
 }
 
